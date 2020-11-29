1ac47f29b1f1a508254d26b631d79269d68dc20b
Majd Hasan <majd.hasan@aperto.com>
2020-11-29 12:43:45 +0100

    add protected routes

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import { Container } from 'reactstrap'
 import { Home, Login, Signup } from './pages'
-import { Navigation } from './components'
+import { Navigation, ProtectedRoute } from './components'
 
 function App() {
   return (
     <div >
       <Navigation></Navigation>
       <Container>
-        <Route path="/" component={Home} exact />
+        <ProtectedRoute path="/" component={Home} exact />
         <Route path="/login" component={Login} exact />
         <Route path="/signup" component={Signup} exact />
       </Container>
--- src/actions/types.js
+++ src/actions/types.js
@@ -1,9 +1,11 @@
+// ------------- Auth actions ------------------
 export const AUTH_ATTEMPTING = "AUTH_ATTEMPTING";
 export const AUTH_SUCCESS = "AUTH_SUCCESS";
 export const AUTH_FAILED = "AUTH_FAILED";
 export const USER_LOGGED_OUT = "USER_LOGGED_OUT";
+export const PROFILE_FETCHED = "PROFILE_FETCHED";
+
+// ------------- Registering actions ----------
 export const SIGNUP_ATTEMPTING = "SIGNUP_ATTEMPTING";
 export const SIGNUP_SUCCESS = "SIGNUP_SUCCESS";
 export const SIGNUP_FAILED = "SIGNUP_FAILED";
-export const PROFILE_FETCHED = "PROFILE_FETCHED";
-
